6f85380afff7e0b09955719ee33ddf7750f5e070
Miguel Walisson <miguelwalisson098@gmail.com>
2025-07-30 16:48:47 -0300

    dev
    Implementação Spring Security

--- clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/security/SecurityFilter.java
+++ clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/security/SecurityFilter.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.token.TokenService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.API.clinicaMedica.Repository.UsuarioRepository;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -20,20 +21,21 @@ public class SecurityFilter extends OncePerRequestFilter {
      @Autowired
     TokenService tokenService;
     @Autowired
-    UserRepository userRepository;
+    private UsuarioRepository usuarioRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null){
             var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByLogin(login);
+            UsuarioRepository user = UsuarioRepository.findByCpf(cpf);
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response); 
     }
+
 
     private String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
