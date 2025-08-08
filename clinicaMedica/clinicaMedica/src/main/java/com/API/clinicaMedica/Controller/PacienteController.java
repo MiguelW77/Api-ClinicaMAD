f4a7d2a3f94914c7c72560e6c9a47ac836e329fd
Miguel Walisson <miguelwalisson098@gmail.com>
2025-08-08 10:32:51 -0300

    update rotas

--- clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/Controller/PacienteController.java
+++ clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/Controller/PacienteController.java
@@ -30,7 +30,7 @@ public class PacienteController {
         return service.listarTodos();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/usuario")
     public ResponseEntity<PacienteModel> buscarPorId(@PathVariable Long id) {
         PacienteModel paciente = service.buscarPorId(id);
         return paciente != null ? ResponseEntity.ok(paciente) : ResponseEntity.notFound().build();
@@ -41,7 +41,7 @@ public class PacienteController {
         return service.salvar(paciente);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/usuario")
     public ResponseEntity<PacienteModel> atualizar(@PathVariable Long id, @RequestBody PacienteModel paciente) {
         try {
             return ResponseEntity.ok(service.atualizar(id, paciente));
@@ -50,7 +50,7 @@ public class PacienteController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/usuario")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         try {
             service.deletar(id);
