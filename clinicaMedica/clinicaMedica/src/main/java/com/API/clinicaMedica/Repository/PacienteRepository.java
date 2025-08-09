32db9b6d0f977ff9ea6913c444c9235f7f194c57
Miguel Walisson <miguelwalisson098@gmail.com>
2025-08-09 17:37:30 -0300

    repository edit

--- clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/Repository/PacienteRepository.java
+++ clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/Repository/PacienteRepository.java
@@ -1,5 +1,7 @@
 package com.API.clinicaMedica.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.API.clinicaMedica.Model.PacienteModel;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<PacienteModel, Long> {
-   PacienteModel findByEmailAndSenha(String email, String senha);
+   Optional<PacienteModel> findByEmailAndSenha(String email, String senha);
 }
